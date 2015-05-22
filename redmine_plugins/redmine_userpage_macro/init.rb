9ad92765098ea8d7821d64d4e5dc4c26f148651c
Stanislav German-Evtushenko <ginermail@gmail.com>
2015-05-22 11:40:47 +0300

    redmine_userpage_macro: cosmetic changes
    formatting: remove some spaces, add new lines

--- redmine_plugins/redmine_userpage_macro/init.rb
+++ redmine_plugins/redmine_userpage_macro/init.rb
@@ -16,17 +16,21 @@ Redmine::WikiFormatting::Macros.register do
       project_identifier = args[0].strip
       project_id = Project.find_by_identifier(project_identifier).identifier
     end
-      project_id = @project.identifier unless project_id
-      return nil unless project_id
+    
+    project_id = @project.identifier unless project_id
+    return nil unless project_id
+    
     if args[1] and !args[1].empty?
       user_login = args[1].strip.downcase
     end
+
     if args[2] and !args[2].empty?
       parent_page = args[2].strip
     end
-      user_login = User.current.login unless user_login
-      userpage_exists = Wiki.find_page(project_id + ":" + user_login)
-      url_class = 'wiki-page' + (userpage_exists ? '' : ' new')
-      h(link_to(user_login, {:controller => 'wiki', :action => 'show', :project_id => project_id, :id => user_login, :parent => parent_page}, :class => url_class))
+
+    user_login = User.current.login unless user_login
+    userpage_exists = Wiki.find_page(project_id + ":" + user_login)
+    url_class = 'wiki-page' + (userpage_exists ? '' : ' new')
+    h(link_to(user_login, {:controller => 'wiki', :action => 'show', :project_id => project_id, :id => user_login, :parent => parent_page}, :class => url_class))
   end
 end
